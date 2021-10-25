224f52684a664729d9bcff8654aa05cdfe6db89b
jaeguin <gagle@gmail.com>
2021-10-26 00:24:55 +0900

    v2 code convention

--- src/client/Client.java
+++ src/client/Client.java
@@ -8,50 +8,69 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
+import static java.lang.Thread.sleep;
 
 public class Client {
-    public static void main(String[] args) {
 
-        while (true) {
-            try {
-                Socket sock = new Socket();
-                sock.connect(new InetSocketAddress("localhost", 3070));
-                System.out.println("서버접속성공!");
+	public static void file_detector() {
+		while (true) {
+			try {
+				sleep(3000);
 
-                Scanner scan = new Scanner(System.in);
-                System.out.print("전송할 파일명 입력 : ");
+				System.out.println("파일 검색중...");
+				File dir = new File("C:\\client");
+				File files[] = dir.listFiles();
 
-                String fileName = scan.next();
-                File f = new File(fileName);
+				if (files.length != 0) {
+					System.out.println("새로운 파일이 있습니다.");
+					file_sender();
+				}
+			} catch (InterruptedException | IOException e) {
+			}
+		}
+	}
 
+	public static void file_sender() throws IOException {
+		File dir = new File("C:\\client");
+		File files[] = dir.listFiles();
 
-                //데이터를 전송하기 위한 스트림객체 생성하기
-                DataOutputStream dos = new DataOutputStream(sock.getOutputStream());
+		for (int i = 0; i < files.length; i++) {
 
-                //제일먼저 파일명 보내기
-                dos.writeUTF(f.getName());
-                dos.flush();
-                byte b[] = new byte[1024];
-                int n = 0;
+			Socket sock = new Socket();
+			sock.connect(new InetSocketAddress("localhost", 3070));
+			System.out.println("서버접속성공!");
 
-                //파일에서 읽어오기 위한 스트림객체 생성
-                FileInputStream fis = new FileInputStream(fileName);
-                long fileSize = 0;
+			// 데이터를 전송하기 위한 스트림객체 생성하기
+			DataOutputStream dos = new DataOutputStream(sock.getOutputStream());
+			String filenames = files[i].getName();
+			System.out.println("전송할 파일 이름 : " + filenames);
 
-                while ((n = fis.read(b)) != -1) {//파일에서 읽어와 b에 저장
-                    dos.write(b, 0, n);//서버에 파일에서읽어온 데이터 보내기
-                    fileSize += n;//파일크기 구하기
-                }
-                dos.close();//스트림닫기
-                fis.close();//스트림닫기
-                System.out.println(fileSize + "bytes 크기의 파일전송 완료!");
-                sock.close();//소켓닫기
-            } catch (UnknownHostException ue) {
-                System.out.println(ue.getMessage());
-            } catch (IOException ie) {
-                System.out.println(ie.getMessage());
-            }
-        }
-    }
+			// 제일먼저 파일명 보내기
+			dos.writeUTF(filenames);
+			dos.flush();
+			System.out.println(files[i] + " 전송 시작");
 
+			int n = 0;
+			byte b[] = new byte[1024];
+
+			// 파일에서 읽어오기 위한 스트림객체 생성
+			FileInputStream fis = new FileInputStream(files[i]);
+
+			long fileSize = 0;
+			while ((n = fis.read(b)) != -1) {// 파일에서 읽어와 b에 저장
+				dos.write(b, 0, n);// 서버에 파일에서읽어온 데이터 보내기
+				fileSize += n;// 파일크기 구하기
+			}
+			dos.close();// 스트림닫기
+			fis.close();// 스트림닫기
+			System.out.println(fileSize + "bytes 크기의 파일전송 완료!\n");
+
+			sock.close();// 소켓닫기
+			files[i].delete();
+		}
+	}
+
+	public static void main(String[] args) {
+		file_detector();
+	}
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -6,47 +6,44 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 public class Server {
-    public static void main(String[] args) {
+	public static void file_receiver() {
+		ServerSocket server = null;
 
-        ServerSocket server=null;
+		while (true) {
+			try {
+				server = new ServerSocket(3070);
+				System.out.println("클라이언트 접속대기중...");
+				Socket sock = server.accept();
+				System.out.println("클라이언트 접속!");
+				// 클라이언트가 보내온 데이터를 읽어오기위한 스트림객체 생성
+				DataInputStream dis = new DataInputStream(sock.getInputStream());
+				// 제일먼저 보내온 파일명 읽어오기
+				String fileName = dis.readUTF();
+				// 전송된 파일명으로 파일생성하기
+				FileOutputStream fos = new FileOutputStream("c:\\server\\" + fileName);
+				byte[] b = new byte[1024];
+				int n = 0;
+				long fileSize = 0;
+				while ((n = dis.read(b)) != -1) {// 전송된 데이터 읽어와 b배열에 저장
+					// b배열의 데이터를 파일로 저장
+					fos.write(b, 0, n);
+					fileSize += n;// 전송된 파일크기 구하기
+				}
+				// 스트림닫기
+				fos.close();
+				dis.close();
 
-        while(true) {
-            try {
-                server = new ServerSocket(3070);
-                System.out.println("클라이언트 접속대기중...");
-                Socket sock = server.accept();
-                System.out.println("클라이언트 접속!");
-                //클라이언트가 보내온 데이터를 읽어오기위한 스트림객체 생성
-                DataInputStream dis = new DataInputStream(sock.getInputStream());
-                //제일먼저 보내온 파일명 읽어오기
-                String fileName = dis.readUTF();
-                //전송된 파일명으로 파일생성하기
-                FileOutputStream fos = new FileOutputStream("c:\\server\\" + fileName);
-                byte[] b = new byte[1024];
-                int n = 0;
-                long fileSize = 0;
-                while ((n = dis.read(b)) != -1) {//전송된 데이터 읽어와 b배열에 저장
-                    //b배열의 데이터를 파일로 저장
-                    fos.write(b, 0, n);
-                    fileSize += n;//전송된 파일크기 구하기
-                }
-                //스트림닫기
-                fos.close();
-                dis.close();
+				// 소켓닫기
+				sock.close();
+				server.close();
+				System.out.println("c:\\server폴더에 " + fileSize + " bytes 크기의 파일수신 완료!");
+			} catch (IOException ie) {
+			}
+		}
+	}
 
-
-                //소켓닫기
-                sock.close();
-                server.close();
-                System.out.println("c:\\server폴더에 " + fileSize +
-                        " bytes 크기의 파일수신 완료!");
-            } catch (IOException ie) {
-                //System.out.println(ie.getMessage());
-                //ie.printStackTrace();
-
-            }
-        }
-    }
+	public static void main(String[] args) {
+		file_receiver();
+	}
 }
